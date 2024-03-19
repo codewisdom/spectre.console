453359ef16936411c216632e4c007ca9538529c5
Sam <samuel.l.sellars@gmail.com>
2024-03-19 15:04:38 -0700

    Add non-generic base IPrompt interface

--- src/Spectre.Console/Prompts/IPrompt.cs
+++ src/Spectre.Console/Prompts/IPrompt.cs
@@ -4,7 +4,7 @@ namespace Spectre.Console;
 /// Represents a prompt.
 /// </summary>
 /// <typeparam name="T">The prompt result type.</typeparam>
-public interface IPrompt<T>
+public interface IPrompt<T> : IPrompt
 {
     /// <summary>
     /// Shows the prompt.
@@ -20,4 +20,11 @@ public interface IPrompt<T>
     /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
     /// <returns>The prompt input result.</returns>
     Task<T> ShowAsync(IAnsiConsole console, CancellationToken cancellationToken);
+}
+
+/// <summary>
+/// Provides a non-generic base interface for all prompts.
+/// </summary>
+public interface IPrompt
+{
 }
